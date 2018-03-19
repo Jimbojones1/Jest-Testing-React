5a4dab3207e2f825c835ad615db9c18c87675b4a
Jim Haff <Jimmyhaff3@gmail.com>
2018-03-19 11:41:02 -0500

    created test for numbers arrays and objects

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,1 +1,51 @@
-import data from '../../data.courses.json';
+import data from '../../data/courses.json';
+
+// Testing Numbers
+const numItems = data.length;
+
+test('Number of Items = 12', () => {
+  expect(numItems).toBe(12);
+});
+
+
+// Testing Strings
+const dataTest = data[0].title;
+
+test('There is a JS in this title', () => {
+  // match is looking for a JS in the string
+  expect(dataTest).toMatch(/JS/);
+});
+
+test('The title contains React', () => {
+  expect(dataTest).toContain('React');
+});
+
+// Testing Arrays
+const data2 = ['React Native', 'MeteorJs'];
+
+test('The List will contain React Native and MeteorJs', () => {
+  expect(['React Native', 'MeteorJs', 'React']).toEqual(expect.arrayContaining(data2));
+});
+
+
+// Testing Objects
+
+const data3 = data[0];
+
+test('The first course to have a property of title', () => {
+  expect(data3).toHaveProperty('title');
+});
+
+
+test('The first course to have a property of title', () => {
+  expect(data3).toHaveProperty('views', '31,266');
+});
+
+
+
+
+
+
+
+
+
